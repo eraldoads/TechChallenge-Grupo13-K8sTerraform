253fd50b9fad63fdda0a200235201d981509317a
Luís Felipe Amengual Tatsch <lfelipe_at@yahoo.com.br>
2024-03-03 09:59:18 -0300

    Configurando pipeline tf

--- data.tf
+++ data.tf
@@ -2,6 +2,6 @@ data "aws_db_instance" "database" {
 }
 
 data "aws_ecr_image" "piklesfastfood_image" {
-  repository_name = var.projectName
+  repository_name = "ecr-${var.projectName}"
   image_tag       = "latest"
 }
