017f0a59c50cf66aa9e3c07e14d8a4a74d4f8af7
Luís Felipe Amengual Tatsch <lfelipe_at@yahoo.com.br>
2024-05-16 18:48:23 -0300

    Infra pagamento

---
+++ sg_pagamento.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "sg_pagamento" {
+  name        = "sgpagamento"
+  description = "sg-pagamento"
+  vpc_id      = var.vpcId
+
+  ingress {
+    description = "pikles"
+    from_port   = 80
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
